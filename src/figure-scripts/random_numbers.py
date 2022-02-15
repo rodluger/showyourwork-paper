7b1e9552ec4fbcd6ce9abfd86b5c8626b0b52622
Rodrigo Luger <rodluger@gmail.com>
2022-02-15 12:34:23 -0500

    debug

--- src/figure-scripts/random_numbers.py
+++ src/figure-scripts/random_numbers.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 data = np.loadtxt(paths.data / "random_numbers.dat")
-#
+
 fig = plt.figure()
 plt.plot(data)
 fig.savefig(paths.figures / "random_numbers.pdf", format="pdf", bbox_inches="tight")
