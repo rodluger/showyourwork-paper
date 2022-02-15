09aed08c2a0b432d39377e53d10f5e1e925ba9ad
Rodrigo Luger <rodluger@gmail.com>
2022-02-15 11:48:14 -0500

    dummy commit

--- src/figure-scripts/random_numbers.py
+++ src/figure-scripts/random_numbers.py
@@ -4,6 +4,7 @@ import numpy as np
 
 data = np.loadtxt(paths.data / "random_numbers.dat")
 
+#
 fig = plt.figure()
 plt.plot(data)
 fig.savefig(paths.figures / "random_numbers.pdf", format="pdf", bbox_inches="tight")
